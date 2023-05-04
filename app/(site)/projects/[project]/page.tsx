06dd563a0c1e520aaeedfb7b921832c9f347fcd3
nikolayhotim1 <hotim_nikolay@mail.ru>
2023-05-05 00:07:29 +0300

    feature: add navbar, some pages, improve styles

--- app/(site)/projects/[project]/page.tsx
+++ app/(site)/projects/[project]/page.tsx
@@ -2,11 +2,7 @@ import { getProject } from '@/sanity/sanity-utils'
 import { PortableText } from '@portabletext/react'
 import Image from 'next/image'
 
-type Props = {
-	params: { project: string }
-}
-
-export default async function Project({ params }: Props) {
+export default async function Project({ params }: { params: { project: string } }) {
 	const slug = params.project
 	const project = await getProject(slug)
 	return (
